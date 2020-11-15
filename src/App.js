ddac1ac82a54ec3263e666ad8e394b6dc4291911
Geethu Gopi <geethugopi33@gmail.com>
2020-11-16 02:37:17 +1300

    App.js and list.js are modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import React, { useState } from 'react';
 import data from './data';
-
+import List from './list';
 function App() {
   const [people, setPeople] = useState(data);
   console.log(people);
   return (
     <main>
-    <section>
-    <h2>{people.length} birthdays today</h2>
+    <section className = 'container'>
+    <h2 >{people.length} birthdays today</h2>
+    <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
     </section>
     </main>
   );
